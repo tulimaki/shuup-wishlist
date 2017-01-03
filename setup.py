a03d8e164b82a2b5ba40891d1b99b9de79696cfd
Panu Tulimäki <panu@shuup.com>
2017-01-03 11:30:04 -0800

    Bump to version 0.3.1 with Shuup 1.0.0 support

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ except ImportError:
 if __name__ == '__main__':
     setuptools.setup(
         name="shuup_wishlist",
-        version="0.3.0",
+        version="0.3.1",
         description="Shuup Wishlist",
         packages=setuptools.find_packages(),
         include_package_data=True,
         entry_points={"shuup.addon": "shuup_wishlist=shuup_wishlist"},
         cmdclass=(shuup_setup_utils.COMMANDS if shuup_setup_utils else {}),
         install_requires=[
-            'shuup>=0.5,<1',
+            'shuup>=0.5',
         ],
     )
