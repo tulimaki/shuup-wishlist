ceb717002ee05ffdad58339c0e3c893bdea5ca7f
Panu Tulimäki <panu@shuup.com>
2019-04-11 14:46:16 -0700

    Update license headers

--- shuup_wishlist/favorites.py
+++ shuup_wishlist/favorites.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# This file is part of Shuup Wishlist.
+# This file is part of Shuup.
 #
-# Copyright (c) 2012-2018, Shoop Commerce Ltd. All rights reserved.
+# Copyright (c) 2012-2019, Shoop Commerce Ltd. All rights reserved.
 #
-# This source code is licensed under the AGPLv3 license found in the
+# This source code is licensed under the OSL-3.0 license found in the
 # LICENSE file in the root directory of this source tree.
 from django.utils.translation import ugettext_lazy as _
 
